96e3b318c89c261ee7aa992928f751b831b24d39
mrempala <mrempala@users.noreply.github.com>
2014-03-28 16:09:13 -0700

    Added PCB Struct

--- simcomp.c
+++ simcomp.c
@@ -1,14 +1,33 @@
 //Header Files
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+
+#define true 1
+#define false 0
 
 //Global
+
+struct processControlBlock
+{
+    //Identical to slide from design
+    pid_t pid;
+    pid_t parentPid;
+    //Time of arrival
+    //Time remaining
+    int priority;
+    //List of operations
+};
+
 
 //Function Declarations
 
 //Main
 int main()
 {
+
     return 0;
 }
 
 //Function Implementations
-//test edit
+
